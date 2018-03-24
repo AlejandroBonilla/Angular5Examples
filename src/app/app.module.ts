c6eb62f1b99430a91df088b92d103cb10bcc156b
thevi <thevilos@gmail.com>
2018-03-24 05:09:15 -0300

    ahora si

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'// importar ngmodel
 
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-<<<<<<< HEAD
-
-=======
->>>>>>> a038f5879cc00c7fa8a55c4d22f3e72bab539a28
 
 @NgModule({
   declarations: [
